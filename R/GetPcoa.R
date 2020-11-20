0895c7eb5d16af48bd31e7792a8d46504fb4a2d7
qdu-bioinfo <qdu_bioinfo@163.com>
2020-11-20 12:45:54 +0800

    update files

--- R/GetPcoa.R
+++ R/GetPcoa.R
@@ -1,3 +1,15 @@
+#' PCoA Implementation
+#'
+#' @param dist_matrix numeric matrix indicating the pairwise distance matrix
+#' @param k numeric variable indicating the number of dimension coordinate system (e.g. k = 2 or 3 space), default is 3
+#'
+#' @return the PCoA coordinates matrix
+#'
+#' @export
+#' @examples
+#' data(dist_matrix)
+#' GetPcoa(dist_matrix)
+#' GetPcoa(dist_matrix, k = 3)
 GetPcoa <- function(dist_matrix, k = 3){
 	res = .Call('get_pcoa', dist_matrix, k, PACKAGE = "hrms")
 	return (res)
